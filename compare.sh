2abf9ac01387c3e56d21b9b1128b51b3a030cbc8
networkdave <dave@networkdave.com>
2015-10-02 18:10:28 -0400

    verbage
    changed some comment words

--- compare.sh
+++ compare.sh
@@ -15,7 +15,7 @@ do
 	echo "dig @$server axfr +multiline $line..."
 	dig @$server $line axfr +multiline >> output1.txt
 done
-# Sort the result of output1.txt, remove any line starting with ; or blank lines, save to output1-1.txt
+# Sort output1.txt, remove any line starting with ; or blank lines, save to output1-1.txt
 sort output1.txt | grep -v \; | grep -v "^$" | grep -v ")" > output1-1.txt
 # Wait for user input to repeat the queries
 echo -e "\nPress [Enter] key to repeat the zone transfer after making some change to the data. After the second zone transfer job completes, a comparison will be made and dropped into results.txt"
@@ -27,7 +27,7 @@ do
 	echo "dig @$server axfr +multiline $line..."
 	dig @$server $line axfr +multiline >> output2.txt
 done
-# Sort the result of output1.txt, remove any line starting with ; or blank lines, save to output2-1.txt
+# Sort output1.txt, remove any line starting with ; or blank lines, save to output2-1.txt
 sort output2.txt | grep -v \; | grep -v "^$" | grep -v ")" > output2-1.txt
 # Compare the sorted and cleaned up output files, save to results.txt
 echo `comm -3 output1-1.txt output2-1.txt` > results.txt
